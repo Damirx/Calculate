272009d7336ac74a00e4d4050968ca2439654a93
Alexandr <lord.alex.akl@mail.ru>
2013-07-01 15:10:06 +0600

    Тесты
    Синус и Арксинус

---
+++ TestgGraphrez/AsinTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using graphrez;
+
+namespace TestgGraphrez
+{
+    [TestClass]
+    public class AsinTest
+    {
+        [TestMethod]
+        public void doubleTest()
+        {
+            Arcsinus Calc = new Arcsinus();
+            Assert.AreEqual(1.099, Calc.Calculate(2.0), 0.001);
+            Assert.AreEqual(-1.321, Calc.Calculate(4.0), 0.001);
+        }
+    }
+}
---
+++ TestgGraphrez/Sintest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using graphrez;
+
+namespace TestgGraphrez
+{
+    [TestClass]
+    public class SinTest
+    {
+        [TestMethod]
+        public void DoubleTest()
+        {
+            Sinus calc = new Sinus();
+            Assert.AreEqual(0.909, calc.Calculate(2.0), 0.001);
+            Assert.AreEqual(-0.756, calc.Calculate(4.0), 0.001);
+        }
+    }
+}
