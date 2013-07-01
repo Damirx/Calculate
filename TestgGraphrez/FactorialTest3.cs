acedb4e3009a74917b917283e09e54765e94b1d5
aigul <isina-94@mail.ru>
2013-07-01 15:29:34 +0700

    тест

---
+++ TestgGraphrez/FactorialTest3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using graphrez;
+namespace TestgGraphrez
+{
+    [TestClass]
+    public class FactorialTest3
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Factorial calc = new Factorial();
+            Assert.AreEqual(6, Calculate(3), 0.001);
+
+        }
+    }
+}
---
+++ graphrez/Actg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+        namespace graphrez
+{
+    public class Actg:Operation
+    {
+        public override double Calculate(double arg1)
+        {
+            return (1/(Math.Cos(arg1)/Math.Sin(arg1)));
+        }
+    }
+}
+
