45cc25b05a15db609ead0c9c13fbd71e673f0466
aigul <isina-94@mail.ru>
2013-07-01 16:04:22 +0700

    тест
    факториал

---
+++ TestgGraphrez/FactorialTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using graphrez;
+namespace TestgGraphrez
+{
+    [TestClass]
+    public class FactorialTest
+    {
+        [TestMethod]
+        public void TestInt()
+        {
+            Factorial calc = new Factorial();
+
+            Assert.AreEqual(1307674368000, calc.Calculate(15));
+        
+
+        }
+     }
+}
